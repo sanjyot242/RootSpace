1231a60d8771b88305033624361efb6d2ea4e798
csuftitan <sanjyot.satvi242@gmail.com>
2024-11-05 15:38:21 -0800

    Fixes for deployemnt

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,4 +5,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        entryFileNames: 'index.html',
+      },
+    },
+  },
 })
