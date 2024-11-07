4748f71e106941e37d7416b4c95372c79e704be2
Sanjyot <41758207+sanjyot242@users.noreply.github.com>
2024-11-07 14:28:16 -0800

    :Updated ShoppingCart Page

--- frontend/src/components/CartItem/CartItem.tsx
+++ frontend/src/components/CartItem/CartItem.tsx
@@ -24,15 +24,15 @@ const CartItem: React.FC<CartItemProp> = ({ quantity, size, products }) => {
   return (
     <div className='flex items-start gap-32 self-stretch'>
       <div className='flex items-start gap-8 md:gap-32 md:flex-1 md:flex-shrink-0 md:basis-0'>
-        <div className='flex-shrink-0 w-[145px] h-[215px] md:flex-1 md:flex-shrink-0 md:basis-0 md:self-stretch rounded-xxs'>
+        <div className='flex-shrink-0 w-[145px] h-[215px] md:w-[276px] md:h-[239px] md:flex-1 md:flex-shrink-0 md:basis-0 md:self-stretch '>
           <img
             src={products.image}
             alt='This is product image'
-            className='bg-cover bg-no-repeat'
+            className='w-full h-full object-cover  rounded-xxs'
           />
         </div>
-        <div className='flex flex-col items-start gap-8 flex-1 flex-shrink-0 basis-0 md:gap-16'>
-          <div className='flex flex-col items-start self-stretch gap-8'>
+        <div className='flex flex-col   gap-16 flex-1 flex-shrink-0 basis-0 md:gap-32 '>
+          <div className='flex flex-col items-start self-stretch gap-8 '>
             {/* for product details */}
             <span className='text-shades-black font-ebgaramond font-medium text-mobile-headings-h6-medium md:text-desktop-headings-h6-medium'>
               {products.name}
@@ -40,49 +40,58 @@ const CartItem: React.FC<CartItemProp> = ({ quantity, size, products }) => {
             <span className='text-text-tertiary font-ebgaramond font-normal text-paragraph-medium-regular md:text-paragraph-medium-regular'>
               Description
             </span>
+            <span className='text-text-tertiary font-ebgaramond font-normal text-paragraph-medium-regular md:text-paragraph-medium-regular'>
+              Product ID
+            </span>
+
             <span className='text-shades-black font-ebgaramond font-medium text-mobile-headings-h6-medium md:hidden w-full '>
               $99
             </span>
           </div>
-          <div className='flex flex-col justify-end self-start gap-8 '>
+          <div className='flex flex-col  self-start gap-8 w-full'>
             {/* for size and stuff */}
-            <div className='flex w-96 h-20 items-center gap-8 md:h-auto'>
+            <div className='flex w-96 h-20 items-center gap-8'>
               <div className='flex h-32 py-8 justify-center items-center gap-8 text-text-tertiary font-ebgaramond font-normal text-paragraph-xsmall-regular md:text-paragraph-medium-regular'>
                 Size
               </div>
-              <Button size='x-small' theme='link' stateProp='normal'>
+              <Button
+                size='small'
+                theme='link'
+                stateProp='normal'
+                className='underline'>
                 Change
               </Button>
             </div>
             <span className='text-text-tertiary font-ebgaramond font-normal text-paragraph-xsmall-regular md:text-paragraph-medium-regular'>
               Quantity
             </span>
-            <div className='flex items-center gap-8 self-stretch md:gap-16'>
+            <div className='flex items-center justify-between gap-8 self-stretch md:gap-16 w-full'>
               <QuantitySelector
                 min={1}
                 max={5}
                 initialQuantity={quantity}
                 onQuantityChange={handleQuantityChange}
               />
-              <Button
-                size='x-small'
-                theme='destructive-link'
-                stateProp='normal'>
-                Delete
-              </Button>
-              <Button size='x-small' theme='link' stateProp='normal'>
-                Save for Later
-              </Button>
+              <div className='flex gap-8'>
+                <Button
+                  size='small'
+                  theme='destructive-link'
+                  stateProp='normal'>
+                  Delete
+                </Button>
+                <Button size='small' theme='link' stateProp='normal'>
+                  Save for Later
+                </Button>
+              </div>
             </div>
           </div>
         </div>
       </div>
       {/* this is div for text price */}
-      <span className='flex-1 flex-shrink-0 basis-0 font-ebgaramond text-shades-black text-right font-medium text-desktop-headings-h6-medium hidden md:block'>
-        $99
-      </span>
 
-      <div></div>
+      <div className='flex-1 flex-shrink-0 basis-0 font-ebgaramond text-shades-black text-right font-medium text-desktop-headings-h6-medium hidden md:block'>
+        $99
+      </div>
     </div>
   );
 };
--- frontend/src/pages/ShoppingCartPage.tsx
+++ frontend/src/pages/ShoppingCartPage.tsx
@@ -1,6 +1,7 @@
 import CloseButton from '../components/Buttons/CloseButton';
 
 import { trendingProducts } from '../../dummyData/data';
+import { EyeIcon } from '../icons/Icon';
 import useWindowDimensions from './../../useWindowDimensions';
 
 import {
@@ -8,6 +9,9 @@ import {
   BreadcrumbItemProps,
 } from '../components/BreadCrumbs/BreadCrumb'; // Adjust the path if needed
 import CartItem from '../components/CartItem/CartItem';
+import { Button } from '../components/Buttons/Button';
+import Card from '../components/Cards/Card';
+import { tr } from 'framer-motion/client';
 
 function ShoppingCartPage() {
   const { width } = useWindowDimensions();
@@ -32,12 +36,63 @@ function ShoppingCartPage() {
           </div>
         )}
 
-        <div className='flex flex-col items-start gap-16 '>
+        <div className='flex flex-col items-start gap-16 w-full '>
           {trendingProducts.map((product, index) => (
-            <div key={index}>
+            <div className='w-full ' key={index}>
               <CartItem products={product} size='S' quantity={1} />
             </div>
           ))}
+        </div>
+
+        {/* Summary Section */}
+        <div className='flex  flex-col items-start gap-16 self-stretch'>
+          <span className='headingsh5medium text-shades-black'>Summary</span>
+          <div className='flex items-start self-stretch gap-32'>
+            <span className='flex-1 flex-shrink-0 basis-0 paragraphsmallregular text-shades-black'>
+              Sub Total
+            </span>
+            <span className='paragraphsmallregular text-shades-black'>
+              $9999
+            </span>
+          </div>
+          <div className='flex  items-center py-16 gap-8 self-stretch border-t-[1px] border-b-[1px] border-t-stroke-primary  border-b-stroke-primary'>
+            <span className='flex-1 flex-shrink-0 basis-0  text-shades-black paragraphsmallmedium'>
+              Bag Total
+            </span>
+            <span className=' text-shades-black paragraphsmallmedium'>
+              $99999
+            </span>
+          </div>
+        </div>
+
+        {/* Buttons */}
+        <div className='flex flex-col items-start gap-16 self-stretch'>
+          <Button
+            size='medium'
+            theme='primary'
+            stateProp='normal'
+            className='w-full'>
+            Proceed to checkout
+          </Button>
+          <Button
+            size='medium'
+            theme='secondary'
+            stateProp='normal'
+            className='w-full'>
+            Shop more ?
+          </Button>
+        </div>
+
+        {/* Cards  */}
+        <div className='flex items-start gap-16 self-stretch'>
+          <Card
+            state='Normal'
+            trigger='Radio Button'
+            showPaymentIcon={false}
+            icon={<EyeIcon />}
+            showButton={false}
+            showDescription={true}
+          />
         </div>
       </div>
     </>
